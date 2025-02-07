157f5e93272318117a3b3225666ab9ba07d7d3f7
milestarricone <milestarricone2@gmail.com>
2025-02-07 18:37:25 -0500

    started work on cards to display profiles

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { ImageBackground } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.card}>
+        <ImageBackground
+          source={{
+            uri: 'https://www.shutterstock.com/shutterstock/photos/2322075567/display_1500/stock-photo-devops-software-development-operations-infinity-symbol-web-development-concept-in-isometric-design-2322075567.jpg',
+
+          }}
+          style={styles.image}
+        >
+        </ImageBackground>
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,4 +28,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  card: {
+    width: '95%',
+    height: '70%',
+    borderRadius: 10,
+    boxShadowColor: '#000',
+    boxShadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    boxShadowOpacity: 1,
+    boxShadowRadius: 10,
+    elevation: 11,
+  }
 });
